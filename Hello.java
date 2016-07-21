4f1e9e8c030469d2d8bba0a8dd9fcc69011f3b5a
XiaoyangZhang <zxytongxin@163.com>
2016-07-21 15:08:34 +0800

    Simple Print

--- Hello.java
+++ Hello.java
@@ -1,13 +1,7 @@
 public class Hello {
-	public static final String COMPANY_NAME = "yootk";
 	public static void main(String[] args) {
-		System.out.println("Happy New Year");
-		System.out.println("Happy Myself");
-		print();
+		System.out.println("*********");
+		System.out.println("#########");
 	}
-	public static void print() {
-		System.out.println("**************");
-		System.out.println("*Hello World!*");
-		System.out.println("**************");
-	}
+
 }
