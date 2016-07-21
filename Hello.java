22aec2d8b43e1921c2fa963e59d1501c790dc730
XiaoyangZhang <zxytongxin@163.com>
2016-07-21 14:16:59 +0800

    Modified Hello.java Files

--- Hello.java
+++ Hello.java
@@ -1,15 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("yootk");
-		int foot = 0;
-		int[] data = new int[data1.length];
-	}
-
-	public static int[] find(int[] temp, int[] check) {
-		for (int i = 0; i <= temp.length; i++) {
-			if (temp[i].indexOf() != -1) {
-				data[foot++] = temp[i];
-			}
-		}
+		System.out.println("Happy New Year");
+		System.out.println("Happy Myself");
 	}
 }
